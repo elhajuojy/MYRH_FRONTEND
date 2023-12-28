b1576acf0420ea53b8d9514de5470e9a97bb5a46
e.elhjoujy <e.elhjoujy@labelvie.ma>
2023-12-28 20:34:08 +0100

    ☕️☕ Admin dashboard  component

---
+++ src/app/interfaces/Agent.ts
@@ -0,0 +1,12 @@
+export interface Agent{
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    address: string;
+    created_at: string;
+    updated_at: string;
+    user_id: number;
+
+}
