9fe898856e7bdfc2d8ef1c8cf39a0934363216e0
e.elhjoujy <e.elhjoujy@labelvie.ma>
2023-12-29 16:38:43 +0100

    ☕️☕ [ company sing up component  ]

--- src/app/features/company/auth/sing-up/company-sing-up.component.ts
+++ src/app/features/company/auth/sing-up/company-sing-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CompanyService} from "../../../../service/company/company.service";
 
 @Component({
   selector: 'sing-up-company',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class CompanySingUpComponent {
 
+
+  constructor(private companyService:CompanyService) {
+  }
+
+  onSingUpSubmit(form:any):void{
+    //
+  }
 }
