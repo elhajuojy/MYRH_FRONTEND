f22591e3edfd2c827206ad0169ea6300de3cbbbf
e.elhjoujy <e.elhjoujy@labelvie.ma>
2023-12-31 20:11:28 +0100

    home component - job-offer-list

---
+++ src/app/shared/job-offer-list/job-offer-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-offer-list',
+  templateUrl: './job-offer-list.component.html',
+  styleUrls: ['./job-offer-list.component.css']
+})
+export class JobOfferListComponent {
+
+}
