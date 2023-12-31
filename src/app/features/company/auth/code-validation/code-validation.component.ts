661fb43387c859266716eb12a2ce11b30520bd28
e.elhjoujy <e.elhjoujy@labelvie.ma>
2023-12-31 17:53:28 +0100

    ☕️☕ [ l'inscription est valable seulement pour les sociétés, chaque inscription passe obligatoirement par un code de validation valable 3 min pour la confirmation de son inscription via un email/sms  ]

--- src/app/features/company/auth/code-validation/code-validation.component.ts
+++ src/app/features/company/auth/code-validation/code-validation.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {CompanyService} from "../../../../service/company/company.service";
 import {error} from "@angular/compiler-cli/src/transformers/util";
+import {ToastService} from "angular-toastify";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -12,34 +14,51 @@ import {error} from "@angular/compiler-cli/src/transformers/util";
 export class CodeValidationComponent {
 
 
+  formCodeValidation:FormGroup = new FormGroup({
+    code: new FormControl('', Validators.required),
+  });
+
+
   constructor(
-    private companyService:CompanyService,
-    private router:ActivatedRoute,
-    private route:Router
-              ) {
-  }
-  handleCodeVerification(event:any) {
-    console.log(event.target.value)
+    private companyService:CompanyService, private router:ActivatedRoute, private route:Router,
+    private _toastService: ToastService
+  ) {}
+
+
+  handleCodeVerification(event:FormGroup) {
+    console.log(event.value.code)
     //:CODE AND THE EMAIL IS ALREADY ON THE URL PARAMS
     let login:string = this.router.snapshot.queryParams["login"]
-    this.companyService.verifyCodeValidation(login,event.target.value).subscribe({
+    this.companyService.verifyCodeValidation(login,event.value.code).subscribe({
       next:(data)=> {
         console.log(data)
         if (data) {
           this.route.navigate(['companies/login'])
+        }else {
+          this._toastService.error(`Error: Invalid code`)
         }
       },
       error:(error)=>{
         //TODO: SHOW ERROR MESSAGE
         console.log(error)
+        this._toastService.error(`Error: ${error.error.message}`)
+
       }
     })
 
 
   }
+  handleResendCode() {
+    this.companyService.sendCodeValidation(this.router.snapshot.queryParams["login"]).subscribe({
+      next:(data)=>{
+        console.log(data)
+        this._toastService.success(`Code was sent to your email`)
+      },
+      error:(error)=>{
+        console.log(error)
+        this._toastService.error(`Error: ${error.error.message}`)
+      }
+    })
 
-
-
-
-
+  }
 }
